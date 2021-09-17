5268621022e72e27f2839b2b58299e9e4f76daae
santibr0 <90932686+santibr0@users.noreply.github.com>
2021-09-17 16:38:22 -0300

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y && git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
-chmd +x build.sh
+chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
 ./build.sh
